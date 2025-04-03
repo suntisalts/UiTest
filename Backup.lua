fc6bc561f3c1bb5bcb70b42f30e928e56e89a89d
suntisalts <suntisalts@gmail.com>
2025-04-03 19:54:46 +0200

    Update Backup.lua

--- Backup.lua
+++ Backup.lua
@@ -1748,7 +1748,7 @@ Components.Element = (function()
 			FontFace = Font.new("rbxasset://fonts/families/GothamSSm.json", Enum.FontWeight.Medium, Enum.FontStyle.Normal),
 			Text = Title,
 			TextColor3 = Color3.fromRGB(240, 240, 240),
-			TextSize = 21,
+			TextSize = 16,
 			TextXAlignment = Enum.TextXAlignment.Left,
 			Size = UDim2.new(1, 0, 0, 14),
 			BackgroundColor3 = Color3.fromRGB(255, 255, 255),
@@ -1762,7 +1762,7 @@ Components.Element = (function()
 			FontFace = Font.new("rbxasset://fonts/families/GothamSSm.json"),
 			Text = Desc,
 			TextColor3 = Color3.fromRGB(200, 200, 200),
-			TextSize = 18,
+			TextSize = 14,
 			TextWrapped = true,
 			TextXAlignment = Enum.TextXAlignment.Left,
 			BackgroundColor3 = Color3.fromRGB(255, 255, 255),
@@ -6768,4 +6768,3 @@ end
 
 task.wait(0.1)
 return Library, SaveManager, InterfaceManager, Mobile
-
